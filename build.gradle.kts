8e849436d4cd566ecc671bfd5319bbb93de851de
Olivier Le Merdy <olivier.lemerdy@kry.se>
2024-03-04 15:45:20 +0100

    Jacoco configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,3 +56,11 @@ tasks.withType<KotlinCompile> {
 tasks.withType<Test> {
     useJUnitPlatform()
 }
+
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test) // tests are required to run before generating the report
+}
